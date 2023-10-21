968ed83b08f77117b41a31248d427fb8e0deb1ac
Hayder Abu AlHummos <greekrifleopens@gmail.com>
2023-10-21 22:07:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const findMatching = (source, sought) => {
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+}
+
+const fuzzyMatch = (source, sought) => {
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase().indexOf(sought.toLowerCase()) === 0
+    );
+}
+
+const matchName = (source, sought) => {
+    return source.filter(
+        (record => record.name === sought)
+    );
+}
